3f2a7b3e37a399169b4ec44af6fe8117eea6f4ba
Tanuj Chauhan <tanujchauhan76@gmail.com>
2025-05-06 21:53:26 -0600

    first commit

---
+++ Codingexercise/src/test/java/com/codewithtanuj/Codingexercise/CodingexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithtanuj.Codingexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
